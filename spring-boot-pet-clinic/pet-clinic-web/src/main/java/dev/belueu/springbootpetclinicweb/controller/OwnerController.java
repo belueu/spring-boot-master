4672d2bac23dcd68488a64c984e1817df0777ee7
belueu <belu.development@gmail.com>
2022-02-21 01:40:33 +0200

    Added html template to display a list of owners

--- spring-boot-pet-clinic/pet-clinic-web/src/main/java/dev/belueu/springbootpetclinicweb/controller/OwnerController.java
+++ spring-boot-pet-clinic/pet-clinic-web/src/main/java/dev/belueu/springbootpetclinicweb/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package dev.belueu.springbootpetclinicweb.controller;
 
+import dev.belueu.springbootpetclinicdata.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"", "/","/index", "/index.html"})
-    public String listOwners() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
