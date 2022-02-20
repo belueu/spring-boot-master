b1146013f97d697c296712fd35f2999296520fb5
belueu <belu.development@gmail.com>
2022-02-20 21:54:53 +0200

    Added Owners Index Page and Controller to display the template

--- spring-boot-pet-clinic/pet-clinic-web/src/main/java/dev/belueu/springbootpetclinicweb/controller/VetController.java
+++ spring-boot-pet-clinic/pet-clinic-web/src/main/java/dev/belueu/springbootpetclinicweb/controller/VetController.java
@@ -5,9 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String listVets(Model model) {
 //        model.addAttribute("vets", )
         return "vets/index";
