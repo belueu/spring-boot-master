245cc620d4f48424876e37859dba77bcab94dc00
belueu <belu.development@gmail.com>
2022-02-28 11:21:57 +0200

    Specified test class in spring-boot-dependency-injection project and fixed the package/build for spring-boot-master project
    Removed unnecessary css package and refactored base layout, owners/index, vets/index

--- spring-boot-dependency-injection/src/test/java/dev/belueu/springbootdependencyinjection/SpringBootDependencyInjectionApplicationTests.java
+++ spring-boot-dependency-injection/src/test/java/dev/belueu/springbootdependencyinjection/SpringBootDependencyInjectionApplicationTests.java
@@ -3,7 +3,7 @@ package dev.belueu.springbootdependencyinjection;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {SpringBootDependencyInjectionApplication.class})
 class SpringBootDependencyInjectionApplicationTests {
 
     @Test
