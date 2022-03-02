9168de1f2c65d0297113522a0b5e6547444f8e37
belueu <belu.development@gmail.com>
2022-03-02 15:49:03 +0200

    Added PetTypeMapService implementation

---
+++ spring-boot-pet-clinic/pet-clinic-data/src/main/java/dev/belueu/springbootpetclinicdata/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package dev.belueu.springbootpetclinicdata.service;
+
+import dev.belueu.springbootpetclinicdata.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
