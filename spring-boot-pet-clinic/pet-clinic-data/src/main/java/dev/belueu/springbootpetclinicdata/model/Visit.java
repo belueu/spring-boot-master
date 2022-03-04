ea8ee29dfc8e47bf13ecc9f61552ae5fdb309d5b
belueu <belu.development@gmail.com>
2022-03-05 00:50:47 +0200

    Mapped Visit as a JPA entity and added reference in Pet for visits

--- spring-boot-pet-clinic/pet-clinic-data/src/main/java/dev/belueu/springbootpetclinicdata/model/Visit.java
+++ spring-boot-pet-clinic/pet-clinic-data/src/main/java/dev/belueu/springbootpetclinicdata/model/Visit.java
@@ -1,13 +1,18 @@
 package dev.belueu.springbootpetclinicdata.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id", referencedColumnName = "id")
     private Pet pet;
 
     public LocalDate getDate() {
