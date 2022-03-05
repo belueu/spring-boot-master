205343d1c930b1a70a258473884f5521538d42f6
belueu <belu.development@gmail.com>
2022-03-05 23:14:59 +0200

    Added VistMapService

---
+++ spring-boot-pet-clinic/pet-clinic-data/src/main/java/dev/belueu/springbootpetclinicdata/service/VisitService.java
@@ -0,0 +1,6 @@
+package dev.belueu.springbootpetclinicdata.service;
+
+import dev.belueu.springbootpetclinicdata.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
--- spring-boot-pet-clinic/pet-clinic-web/src/main/java/dev/belueu/springbootpetclinicweb/SpringBootPetClinicWebApplication.java
+++ spring-boot-pet-clinic/pet-clinic-web/src/main/java/dev/belueu/springbootpetclinicweb/SpringBootPetClinicWebApplication.java
@@ -37,4 +37,9 @@ public class SpringBootPetClinicWebApplication {
     SpecialityService specialityService() {
         return new SpecialityMapService();
     }
+
+    @Bean
+    VisitService visitService() {
+        return new VisitMapService();
+    }
 }
