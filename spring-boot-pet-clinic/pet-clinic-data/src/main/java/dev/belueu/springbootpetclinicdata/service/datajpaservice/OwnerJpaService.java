781ae649a48394cbc5e99780fb6b157bf076ff77
belueu <belu.development@gmail.com>
2022-03-05 22:26:00 +0200

    Added OwnerJpaService layer

---
+++ spring-boot-pet-clinic/pet-clinic-data/src/main/java/dev/belueu/springbootpetclinicdata/service/datajpaservice/OwnerJpaService.java
@@ -0,0 +1,52 @@
+package dev.belueu.springbootpetclinicdata.service.datajpaservice;
+
+import dev.belueu.springbootpetclinicdata.model.Owner;
+import dev.belueu.springbootpetclinicdata.repository.OwnerRepo;
+import dev.belueu.springbootpetclinicdata.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile({"default", "jpaservice"})
+public class OwnerJpaService implements OwnerService {
+
+    private final OwnerRepo ownerRepo;
+
+    public OwnerJpaService(OwnerRepo ownerRepo) {
+        this.ownerRepo = ownerRepo;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return new HashSet<>(ownerRepo.findAll());
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepo.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepo.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepo.deleteById(id);
+    }
+
+    @Override
+    public Optional<Owner> findByLastName(String lastName) {
+        return ownerRepo.findByLastName(lastName);
+    }
+}
