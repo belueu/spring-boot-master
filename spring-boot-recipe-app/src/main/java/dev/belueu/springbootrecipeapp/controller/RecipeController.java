eb4d784f0a2adb2f616f15bb17ade8eb08060781
belueu <belu.development@gmail.com>
2022-03-07 00:20:20 +0200

    Added post method to create new recipe
    Added form for creating new recipe

--- spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/RecipeController.java
+++ spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package dev.belueu.springbootrecipeapp.controller;
 
+import dev.belueu.springbootrecipeapp.command.RecipeCommand;
 import dev.belueu.springbootrecipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
