9cd42aaa0a5a1ac79dd70e94e45aed0a9116de2e
belueu <belu.development@gmail.com>
2022-03-06 12:28:30 +0200

    Added annotation for JUnit 5 in SpringBootPetClinicWebApplicationTest class

--- spring-boot-pet-clinic/pet-clinic-web/src/test/java/dev/belueu/springbootpetclinicweb/SpringBootPetClinicWebApplicationTests.java
+++ spring-boot-pet-clinic/pet-clinic-web/src/test/java/dev/belueu/springbootpetclinicweb/SpringBootPetClinicWebApplicationTests.java
@@ -2,8 +2,11 @@ package dev.belueu.springbootpetclinicweb;
 
 import dev.belueu.springbootpetclinicweb.config.AppConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {AppConfig.class})
 class SpringBootPetClinicWebApplicationTests {
 
