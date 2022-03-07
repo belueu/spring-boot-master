013a30c614d9cc6bd5dd1a53110426d4cf24b1b2
belueu <belu.development@gmail.com>
2022-03-07 02:12:47 +0200

    Added update method in controller
    Added tests and updated index.html

--- spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/IndexController.java
+++ spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/IndexController.java
@@ -4,7 +4,7 @@ import dev.belueu.springbootrecipeapp.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
@@ -16,7 +16,7 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"", "/", "/index"})
+    @GetMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
         log.info("Getting index page.");
         model.addAttribute("recipes", recipeService.getRecipes());
--- spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/RecipeController.java
+++ spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/RecipeController.java
@@ -4,10 +4,10 @@ import dev.belueu.springbootrecipeapp.command.RecipeCommand;
 import dev.belueu.springbootrecipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
@@ -18,17 +18,23 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable Long id, Model model) {
-        model.addAttribute("recipe", recipeService.findById(id));
 
+        model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
     }
 
-    @RequestMapping("/recipe/new")
+    @GetMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
 
+        return "recipe/recipeform";
+    }
+
+    @GetMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable Long id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(id));
         return "recipe/recipeform";
     }
 
@@ -36,6 +42,6 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 }
