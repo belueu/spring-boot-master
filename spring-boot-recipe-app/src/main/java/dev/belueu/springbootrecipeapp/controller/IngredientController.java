0491d9bd7f288d8ce63df04625f18d1eb94cb678
belueu <belu.development@gmail.com>
2022-03-07 11:49:43 +0200

    Added list ingredients view and method to display

---
+++ spring-boot-recipe-app/src/main/java/dev/belueu/springbootrecipeapp/controller/IngredientController.java
@@ -0,0 +1,28 @@
+package dev.belueu.springbootrecipeapp.controller;
+
+import dev.belueu.springbootrecipeapp.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable Long recipeId, Model model) {
+        log.debug("Getting ingredient list from recipe id: " + recipeId);
+
+        model.addAttribute("recipe", recipeService.findCommandById(recipeId));
+
+        return "recipe/ingredient/list";
+    }
+}
---
+++ spring-boot-recipe-app/src/test/java/dev/belueu/springbootrecipeapp/controller/IngredientControllerTest.java
@@ -0,0 +1,50 @@
+package dev.belueu.springbootrecipeapp.controller;
+
+import dev.belueu.springbootrecipeapp.command.RecipeCommand;
+import dev.belueu.springbootrecipeapp.service.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+public class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    void testListIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        //then
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
